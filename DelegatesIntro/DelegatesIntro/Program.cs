19891280fdfbe685e2fd28fd4e32ab4510b3a656
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2024-06-28 10:57:21 -0500

    delegates intro

---
+++ DelegatesIntro/DelegatesIntro/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Numerics;
+
+class Program
+{
+    delegate void LogDel(string msg);
+    static void Main(string[] args)
+    {
+        Log log = new Log();
+        LogDel logTextToScreen = new LogDel(log.LogTextToScreen);
+        LogDel logTextToFile = new LogDel(log.LogTextToFile);
+
+        LogDel multiLog = logTextToScreen + logTextToFile;
+        LogText(multiLog, "message");
+    }
+
+    static void LogText(LogDel logDel,string msg)
+    {
+        logDel(msg);
+    }
+
+}
+
+public class Log
+{
+    public void LogTextToScreen(string msg)
+    {
+        Console.WriteLine($"{DateTime.Now} : {msg}");
+    }
+
+    public void LogTextToFile(string msg)
+    {
+        using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.text"), true))
+        {
+            sw.WriteLine($"{DateTime.Now} : {msg}");
+        }
+    }
+}
